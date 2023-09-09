23f7af95aab2c6701f39a7d1bac54d616579c212
Puneet <puneet19338@gmail.com>
2023-09-09 22:33:31 +0530

    Sign up for

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './Component/Navbar'
+import Signup from './Component/Signup'
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+
+
+
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      
-    </div>
+    <BrowserRouter basename="/app">
+    <Routes>
+    <Route path="/" element={<Navbar />}/>
+    <Route path="/signin" element={<Signup />}/>
+    </Routes>
+  </BrowserRouter>
   )
 }
 
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const Navbar = () => {
+
+    const navigate = useNavigate();
+    const handleClick = () => navigate('/signin');
+
   return (
     <>
       
-<nav className="bg-white border-gray-200 dark:bg-gray-900">
+<nav className="bg-white border-gray-200 dark:bg-gray-900 ">
     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <a href="https://flowbite.com" className="flex items-center">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
@@ -12,7 +30,8 @@ const Navbar = () => {
         </a>
         <div className="flex items-center">
             <a href="tel:5541251234" className="mr-6 text-sm  text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-            <a href="#" className="text-sm bg-blue-500 text-white p-1 rounded-xl  px-6 hover:underline">Login</a>
+            <a  onClick={handleClick} className="text-gray-900 dark:text-white hover:underline">Login</a>
+        
         </div>
     </div>
 </nav>
