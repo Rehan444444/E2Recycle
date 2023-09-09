f897939df21da126451d788008d4dcf765d70c45
Sandeep <sandeep836815@gmail.com>
2023-09-09 02:49:48 -0700

    adding navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Component/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      This is a Home - App page
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      This is a Navbar page
+    </div>
+  )
+}
+
+export default Navbar
